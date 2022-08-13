923050c4b634a88447262ec0ca418a37df861d25
Volodymyr-Bezyk <v.v.bezyk@gmail.com>
2022-08-13 20:13:45 +0300

    simpleLightBox Added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+galleryRef.innerHTML = renderGalleryImages(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  overlayOpacity: 0.8,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function renderGalleryImages(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+console.log();
