c7ad2d3170e1881b33bb45cb1c530b4c68aea481
Volodymyr-Bezyk <v.v.bezyk@gmail.com>
2022-08-14 20:59:26 +0300

    add vimeo lib

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const player = new Player('vimeo-player');
 
+/*
+Підключення плеєра як вказано в ДЗ
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+*/
+
+const LOCALE_STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(e) {
+  const currentTime = e.seconds;
+  const storageCurrentTime = localStorage.setItem(
+    LOCALE_STORAGE_KEY,
+    currentTime
+  );
+  console.log('CURRENT TIME:', Math.round(currentTime));
+}
+
+player.setCurrentTime(localStorage.getItem(LOCALE_STORAGE_KEY));
