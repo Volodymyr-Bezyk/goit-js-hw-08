b703eb0bb10c298af693b1ce171045cc1a9f1cf2
Volodymyr-Bezyk <v.v.bezyk@gmail.com>
2022-08-18 13:12:57 +0300

    default settings to vimeo method

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -22,18 +22,19 @@ function onPlay(e) {
   console.log(`CURRENT TIME: ${Math.round(currentTime)} sec`);
 }
 
-player.setCurrentTime(localStorage.getItem(LOCALE_STORAGE_KEY));
-// .then(function (seconds) {
-//   // seconds = the actual time that the player seeked to
-// })
-// .catch(function (error) {
-//   switch (error.name) {
-//     case 'RangeError':
-//       // the time was less than 0 or greater than the video’s duration
-//       break;
+player
+  .setCurrentTime(localStorage.getItem(LOCALE_STORAGE_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-//     default:
-//       // some other error occurred
-//       break;
-//   }
-// });
+      default:
+        // some other error occurred
+        break;
+    }
+  });
