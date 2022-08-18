33c4d796a679e5e54219ccdf36069f30d1cf3a4e
Volodymyr-Bezyk <v.v.bezyk@gmail.com>
2022-08-18 13:15:53 +0300

    delete promise in vimeo method

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -22,19 +22,4 @@ function onPlay(e) {
   console.log(`CURRENT TIME: ${Math.round(currentTime)} sec`);
 }
 
-player
-  .setCurrentTime(localStorage.getItem(LOCALE_STORAGE_KEY))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(localStorage.getItem(LOCALE_STORAGE_KEY));
